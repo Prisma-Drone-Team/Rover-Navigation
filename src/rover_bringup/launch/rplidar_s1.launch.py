5d28b0ef3d4411a7fafd8e67dfde8440861565fd
quigon96 <stefyx96@hotmail.it>
2024-06-06 17:10:55 +0200

    rplidar launch added to rover_bringup

---
+++ src/rover_bringup/launch/rplidar_s1.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            name='rplidar_composition',
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 256000,
+                'frame_id': 'rover/laser',
+                'inverted': True,
+                'angle_compensate': True,
+            }],
+            remappings=[
+                ('scan', 'rover/scan')
+            ]
+        ),
+    ])
