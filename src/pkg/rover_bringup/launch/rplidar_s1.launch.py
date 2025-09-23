62543ed8aaadb0f2f769f4681e1f11995598e22a
Quigon96 <stefyx96@hotmail.it>
2025-09-23 17:03:50 +0200

    custom obj detection added and custom explorer

--- src/pkg/rover_bringup/launch/rplidar_s1.launch.py
+++ src/pkg/rover_bringup/launch/rplidar_s1.launch.py
@@ -1,27 +1,88 @@
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             name='rplidar_composition',
+#             package='rplidar_ros',
+#             executable='rplidar_composition',
+#             output='screen',
+#             parameters=[{
+#                 'serial_port': '/dev/ttyUSB0',
+#                 'serial_baudrate': 256000,
+#                 'frame_id': 'rover/lidar_link',
+#                 'topic_name': 'scan',
+#                 'inverted': True,
+#                 'angle_compensate': True,
+#             }],
+#             remappings=[
+#                 ('scan', 'rover/scan')
+#             ]
+#         ),
+#     ])
+
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
+    channel_type = LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='256000') 
+    frame_id = LaunchConfiguration('frame_id', default='rover/lidar_link')
+    inverted = LaunchConfiguration('inverted', default='true')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'channel_type',
+            default_value=channel_type,
+            description='Specifying channel type of lidar'),
+        
+        DeclareLaunchArgument(
+            'serial_port',
+            default_value=serial_port,
+            description='Specifying usb port to connected lidar'),
+
+        DeclareLaunchArgument(
+            'serial_baudrate',
+            default_value=serial_baudrate,
+            description='Specifying usb port baudrate to connected lidar'),
+        
+        DeclareLaunchArgument(
+            'frame_id',
+            default_value=frame_id,
+            description='Specifying frame_id of lidar'),
+
+        DeclareLaunchArgument(
+            'inverted',
+            default_value=inverted,
+            description='Specifying whether or not to invert scan data'),
+
+        DeclareLaunchArgument(
+            'angle_compensate',
+            default_value=angle_compensate,
+            description='Specifying whether or not to enable angle_compensate of scan data'),
+
         Node(
-            name='rplidar_composition',
             package='rplidar_ros',
-            executable='rplidar_composition',
-            output='screen',
-            parameters=[{
-                'serial_port': '/dev/ttyUSB0',
-                'serial_baudrate': 256000,
-                'frame_id': 'rover/lidar_link',
-                'topic_name': 'scan',
-                'inverted': True,
-                'angle_compensate': True,
-                'scan_qos.reliability': 'SYSTEM_DEFAULT',
-                'scan_qos.history': 'keep_all',
-                'scan_qos.depth': 100,
-                'scan_qos.durability': 'volatile'
-            }],
+            executable='rplidar_node',
+            name='rplidar_node',
+            parameters=[{'channel_type':channel_type,
+                         'serial_port': serial_port,
+                         'serial_baudrate': serial_baudrate,
+                         'frame_id': frame_id,
+                         'inverted': inverted,
+                         'angle_compensate': angle_compensate}],
             remappings=[
                 ('scan', 'rover/scan')
-            ]
-        ),
+            ],
+            output='screen'),
     ])
+
