88a26ead9265cd9625303c5b5382c0ae84bb328e
Quigon96 <stefyx96@hotmail.it>
2025-09-24 14:03:50 +0200

    object detaction pkg push issue resolved nad clean

---
+++ src/pkg/yolov11_ros2/launch/yolo.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    this_package_name = 'yolov11_ros2'
+    
+    # Run the yolov8 node with OpenVINO
+    yolov11_node = Node(
+        package=this_package_name,
+        executable='yolov11_node',
+        output='screen',
+        parameters=[
+            {'device': 'CPU',  # OpenVINO device
+             'model': 'yolo11n-seg.pt'},
+        ],
+    )
+    
+    return LaunchDescription([
+        yolov11_node,
+    ])
